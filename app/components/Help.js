e220de27dbd68bb98b26aa21feb3aa497086c82e
Adi Davidovich <secret@whydoyou.care>
2020-03-18 18:13:21 +0200

    Add a Help copmonent

---
+++ app/components/Help.js
@@ -0,0 +1,15 @@
+/**
+ * HELP COMPONENT
+ * @author Adi Davidovich
+ * LAST CHANGE: 18/03/2020
+ */
+import React from 'react';
+
+
+export default function Help() {
+    return (
+        <div>
+            Hey from Help Component
+        </div>
+    );
+}
